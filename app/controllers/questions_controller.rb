918e938ca4f521e4cd48e5bb40485b92c1d97609
cd ~/code/$RobertPE96/dotfiles && zsh git_setup.sh <robert.p.eickmeyer@gmail.com>
2022-04-07 18:52:51 +0200

    done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,16 @@
 class QuestionsController < ApplicationController
   def ask
   end
+
+  def answer
+    @message = params[:message]
+    if @message == "I am going to work right now!"
+      return response = ""
+    elsif @message.include?("?")
+      return response = "Silly question, get dressed and got to work"
+    else
+      return response = "I don't core, get dressed and got to work!"
+    end
+  end
+
 end
